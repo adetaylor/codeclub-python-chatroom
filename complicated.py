f0686bef1ec5c519b3b8339776cf721d501b4108
Adrian Taylor <adrian@macrobug.com>
2013-04-07 08:09:29 +0100

    First cut at implementing working network chat room.

---
+++ complicated.py
@@ -0,0 +1,26 @@
+import sys
+import Pyro4
+import select
+import threading
+from participant import Participant
+from message import Message 
+
+sys.excepthook=Pyro4.util.excepthook
+room=Pyro4.Proxy("PYRONAME:example.room")
+room._pyroOneway.add("say")
+daemon = Pyro4.Daemon()
+me=Participant(room)
+uri = daemon.register(me)
+room.add_participant(uri)
+
+def run_daemon():
+  daemon.requestLoop()
+
+t = threading.Thread(target=run_daemon)
+t.daemon = True
+t.start()
+
+while True:
+  text = raw_input("Enter your message: ").strip()
+  message = Message(text)
+  me.say(message)
--- participant.py
+++ participant.py
@@ -1,10 +1,10 @@
 class Participant:
 	def __init__(self, room):
 		self.room = room
-		self.room.add_participant(self)
 	
 	def say(self, msg):
 		self.room.say(msg)
 	
 	def heard(self, msg):
-		print msg + "\n"
+		print "%s\n" % msg.get_msg()
+
